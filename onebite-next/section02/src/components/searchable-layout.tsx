ef2f9cf34259998bef6e871893d4a1db87f4dba3
leecr1215 <leecr1215@naver.com>
2025-03-27 15:33:28 +0900

    DAY4 : 2.8 page layout setting

---
+++ onebite-next/section02/src/components/searchable-layout.tsx
@@ -0,0 +1,56 @@
+import { useRouter } from "next/router";
+import {
+  ChangeEvent,
+  KeyboardEvent,
+  ReactNode,
+  useEffect,
+  useState,
+} from "react";
+
+import style from "./searchable-layout.module.css";
+
+export default function SearchableLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+
+  const q = router.query.q as string;
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
+  const onChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (!search || q === search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
+  return (
+    <div>
+      <div className={style.searchbar_container}>
+        <input
+          value={search}
+          onKeyDown={onKeyDown}
+          onChange={onChangeSearch}
+          placeholder="검색어를 입력하세요 ..."
+          type="text"
+        />
+        <button onClick={onSubmit}>검색</button>
+      </div>
+      {children}
+    </div>
+  );
+}
